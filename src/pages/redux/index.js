bb2557ef7c38ba737d4723f6454eafc6007746d1
WangC <wangcnkym@gmail.com>
2019-04-25 09:53:22 +0800

    Base

--- src/pages/redux/index.js
+++ src/pages/redux/index.js
@@ -7,19 +7,21 @@ import createStoreWithMdware from './store/index';
 import reducers from './reducers/index';
 import App from 'components/app/index';
 
-import Demo from './containers/demo/index'
-
+import Demo from './containers/demo/index';
 
 /**
     @author Mothpro
     这是一个使用react技术搭建的redux演示页面
 **/
-const store = createStoreWithMdware(reducers);
+const store = createStoreWithMdware(
+	reducers,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 ReactDom.render(
-  <Provider store={store}>
-    <App>
-        <Demo/>
-    </App>
-  </Provider>,
-  document.getElementById('container')
+	<Provider store={store}>
+		<App>
+			<Demo />
+		</App>
+	</Provider>,
+	document.getElementById('container')
 );
