69edacfe224849a5e67501395e62edc4c4d6bf2d
WangC <wangcnkym@gmail.com>
2019-04-28 09:37:54 +0800

    Fixed reducers

--- src/pages/business/reducers/index.js
+++ src/pages/business/reducers/index.js
@@ -2,7 +2,6 @@ import { combineReducers } from 'redux';
 import ToolBar from './toolbar/index';
 import TableData from './tableData/index';
 
-// 将现有的reduces加上路由的reducer
 const rootReducer = combineReducers({ ToolBar, TableData });
 
 export default rootReducer;
