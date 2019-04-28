dbc3510f2241495150b17d27a4b8ecdf4b337ba2
WangC <wangcnkym@gmail.com>
2019-04-28 13:27:32 +0800

    remove redux dev tool windows CONST

--- src/pages/business/index.js
+++ src/pages/business/index.js
@@ -9,13 +9,9 @@ import Content from './containers/App/App';
 
 import './index.scss';
 
-// TODO: Clear eslint and Redux devtools extension
-/* eslint-disable no-underscore-dangle */
 const store = createStore(
     reducers,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
-/* eslint-enable */
 ReactDom.render(
     <Provider store={store}>
         <App>
